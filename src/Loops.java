91958fef9d0d0744597cb0c2c877945427bcda90
adityasingh526272 <adityasinghy526@gmail.com>
2026-08-23 11:48:23 +0530

    Initial commit

---
+++ src/Loops.java
@@ -0,0 +1,67 @@
+public class Loops {
+    static void main() {
+
+//        for loop
+//        for (int i=1;i<=10;i++){
+//            System.out.println(i);
+//        }
+//        for (int i=1;i<=10;i+=2){
+//            System.out.println(i);
+//        }
+//        for (int i=1;i<=10;i++){
+//            System.out.println("Aditya");
+//        }
+
+//        Nested loops
+//        for (int i=1;i<=5;i++){
+//            for (int j=1;j<=5;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//        for (int i=1;i<=3;i++) {
+//            for (int j = 1; j <= 3; j++) {
+//                System.out.println("i = " + i + ", j = " + j);
+//            }
+//        }
+
+//        Break and Continue statement
+//        for (int i=1;i<=10;i++){
+//            if (i==6){
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+//       for (int i=1;i<=10;i++){
+//            if (i==5 || i==7){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+//        While Loop
+//        int i=1;
+//        while (i<=6){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        nested while loop
+//        int i=1;
+//        while (i<=3){
+//            int j=1;
+//            while (j<=4){
+//                System.out.println("i=" + i + ", j=" + j);
+//                j++;
+//            }
+//            i++;
+//        }
+
+//        Do while loop
+        int i=1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i>=5);
+    }
+}
