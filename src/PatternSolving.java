3f412f7d4d653e5d1c8b94c8fc6ffefcae19b4d7
adityasingh526272 <adityasinghy526@gmail.com>
2026-08-25 22:33:47 +0530

    Initial commit

--- src/PatternSolving.java
+++ src/PatternSolving.java
@@ -38,15 +38,109 @@ public class PatternSolving {
 //        }
 
 //        Pattern-5
-        int n=5;
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=n-i;j++){
-                System.out.print("  ");
-            }
-            for (int j=1;j<=2*i-1;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+//        int n=5;
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=n-i;j++){
+//                System.out.print("  ");
+//            }
+//            for (int j=1;j<=2*i-1;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+//        Pattern-6
+//        int n=4;
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=i-1;j++){
+//                System.out.print("  ");
+//            }
+//            for (int j=1;j<=2*n-2*i+1;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+//        Pattern-7
+//        int n=4;
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=6;j++){
+//                if (i==1||i==n){
+//                    System.out.print("* ");
+//                }
+//                else {
+//                    if (j==1){
+//                        System.out.print("* ");
+//                    }
+//                    else if (j==6){
+//                        System.out.print("* ");
+//                    }
+//                    else {
+//                        System.out.print("  ");
+//                    }
+//                }
+//            }
+//            System.out.println();
+//        }
+
+//        Pattern-8
+//        int n=5;
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=i;j++){
+//                if (i==3 && j==2){
+//                    System.out.print("  ");
+//                } else if (i==4 && (j==2 || j==3)) {
+//                    System.out.print("  ");
+//                }
+//                else {
+//                    System.out.print("* ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+//        Pattern-9
+//        int n=5;
+//        for (int i=1;i<=n;i++) {
+//            for (int j=1;j<=n-i;j++) {
+//                System.out.print("  ");
+//            }
+//            if (i==1 || i==n) {
+//                for (int j=1;j<=2*i-1;j++) {
+//                    System.out.print("* ");
+//                }
+//            } else {
+//                System.out.print("* ");
+//                for (int j=1;j<=2*i-3;j++) {
+//                    System.out.print("  ");
+//                }
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+//        Pattern=10
+//        int n=4;
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=n-i;j++){
+//                System.out.print("  ");
+//            }
+//            for (int j=1;j<=2*i-1;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//        for (int i=1;i<=n;i++){
+//            if (i==1){
+//                continue;
+//            }
+//            for (int j=1;j<=i-1;j++){
+//                System.out.print("  ");
+//            }
+//            for (int j=1;j<=2*n-2*i+1;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
     }
 }
