86b77b3636ffea7e38f0755a127a62534beb0031
adityasingh526272 <adityasinghy526@gmail.com>
2026-09-04 11:22:41 +0530

    Initial commit

--- src/Transport/Main.java
+++ src/Transport/Main.java
@@ -2,9 +2,15 @@ package Transport;
 
 public class Main {
     static void main(String[] args) {
-        Car c = new Car("Maruti", "800", 4, 5, "Auto");
-        c.startEngine();
-        c.startAC();
-        c.stopEngine();
+//        Car c = new Car("Maruti", "800", 4, 5, "Auto");
+//        c.startEngine();
+//        c.startAC();
+//        c.stopEngine();
+//        System.out.println(c.name);
+
+        Motorcycle m = new Motorcycle("Bullet","600", 2, "U","Hard");
+        m.startEngine();
+        m.wheelie();
+        m.stopEngine();
     }
 }
