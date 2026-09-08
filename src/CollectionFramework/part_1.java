10a7ca6d061e8cdd194ec14c584fbeafca776600
adityasingh526272 <adityasinghy526@gmail.com>
2026-09-08 23:42:59 +0530

    Initial commit

--- src/CollectionFramework/part_1.java
+++ src/CollectionFramework/part_1.java
@@ -9,71 +9,76 @@ public class part_1 {
         //Arraylist -> concrete class
 //       ArrayList<Integer> list = new ArrayList<>();
 //        List<Integer> list = new LinkedList<>();
-        List<Integer> list = new Vector<>();
-       //add
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        System.out.println(list);
-        list.add(40);
-        System.out.println(list);
-        list.remove(2);
-        System.out.println(list);
+//        List<Integer> list = new Vector<>();
+//        List<Integer> list = new Stack<>();
+
+        //add
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        System.out.println(list);
+//        list.add(40);
+//        System.out.println(list);
+//        list.remove(2);
+//        System.out.println(list);
 
         //addAll
 //        List<Integer> list2 = new ArrayList<>();
 //        List<Integer> list2 = new LinkedList<>();
-        List<Integer> list2 = new Vector<>();
-        list2.add(101);
-        list2.add(102);
-        list2.add(50);
-        list.addAll(list2);
-        System.out.println(list);
-        list.removeAll(list2);
-        System.out.println(list);
+//        List<Integer> list2 = new Vector<>();
+//        List<Integer> list2 = new Stack<>();
+//        list2.add(101);
+//        list2.add(102);
+//        list2.add(50);
+//        list.addAll(list2);
+//        System.out.println(list);
+//        list.removeAll(list2);
+//        System.out.println(list);
 
         //size
-        System.out.println(list.size());
+//        System.out.println(list.size());
 
         //clear
-        System.out.println("Printing list2 : " + list2);
-        list2.clear();
-        System.out.println(list2.size());
+//        System.out.println("Printing list2 : " + list2);
+//        list2.clear();
+//        System.out.println(list2.size());
 
-        //i want to traverse list using iterator
-        Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()){
-            System.out.println("Element : " + iterator.next());
-        }
+//        i want to traverse list using iterator
+//        Iterator<Integer> iterator = list.iterator();
+//        while (iterator.hasNext()){
+//            System.out.println("Element : " + iterator.next());
+//    }
 
 //        List<Integer> list3 = new LinkedList<>();
-        List<Integer> list3 = new Vector<>();
-        list3.add(11);
-        list3.add(12);
-        list3.add(14);
-        System.out.println(list3.get(0));
-        System.out.println("Before set : " + list3);
-        list3.set(0,100);
-        System.out.println("After set : " + list3);
+//        List<Integer> list3 = new Vector<>();
+//        List<Integer> list3 = new Stack<>();
+//        list3.add(11);
+//        list3.add(12);
+//        list3.add(14);
+//        System.out.println(list3.get(0));
+//        System.out.println("Before set : " + list3);
+//        list3.set(0,100);
+//        System.out.println("After set : " + list3);
 
 //        Collection<Integer> collection = new ArrayList<>();
 
         //toArray
-        Object[] arr = list3.toArray();
-        for (Object obj: arr){
-            System.out.println(obj);
-        }
+//        Object[] arr = list3.toArray();
+//        for (Object obj: arr){
+//            System.out.println(obj);
+//        }
 
         //Contains
-        System.out.println(list3.contains(100));
+//        System.out.println(list3.contains(100));
 
-        list.add(6);
-        list.add(2);
-        System.out.println("Printing entire list : " + list);
-
-        //Sort an arraylist
-        Collections.sort(list);
-        System.out.println("Printing entire list : " + list);
+//        list.add(6);
+//        list.add(2);
+//        System.out.println("Printing entire list : " + list);
+//
+//        //Sort an arraylist
+//        System.out.println("Sorting : ");
+//        Collections.sort(list);
+//        System.out.println("Printing entire list : " + list);
 
 //        ArrayList<Integer> newList = (ArrayList<Integer>)list.clone();
 //        LinkedList<Integer> newList = (LinkedList<Integer>)list.clone();
@@ -83,22 +88,22 @@ public class part_1 {
 //        marks.ensureCapacity(100);
 
 //        System.out.println(marks.isEmpty());
-        list.add(30);
-        list.add(60);
-        list.add(24);
-        System.out.println("printing original list : " + list);
-        System.out.println(list.lastIndexOf(5));
+//        list.add(30);
+//        list.add(60);
+//        list.add(24);
+//        System.out.println("printing original list : " + list);
+//        System.out.println(list.lastIndexOf(5));
 
 //        System.out.println(list.indexOf(20));
 //        System.out.println(newList.indexOf(20));
 
         //add first and last
-        list.remove(7);
-        list.remove(6);
-        list.remove(5);
-        list.remove(4);
-        list.remove(3);
-        System.out.println("printing original list : " + list);
+//        list.remove(7);
+//        list.remove(6);
+//        list.remove(5);
+//        list.remove(4);
+//        list.remove(3);
+//        System.out.println("printing original list : " + list);
 //        list.addFirst(101);
 
 //        LinkedList<Integer> l1 = new LinkedList<>();
@@ -125,5 +130,24 @@ public class part_1 {
 //        l1.removeLast();
 //        System.out.println(l1);
 
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        System.out.println(st);
+        st.push(11);
+        System.out.println(st);
+        st.push(12);
+        System.out.println(st);
+
+        st.pop();
+        System.out.println(st);
+
+        System.out.println(st.peek());
+
+        System.out.println(st.search(10));
+
+        System.out.println(st.empty());
+
+
     }
+
 }
