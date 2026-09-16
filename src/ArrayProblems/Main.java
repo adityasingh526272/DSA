4533ebb3dfabd075083f39d4a1d7c22cd4478c32
adityasingh526272 <adityasinghy526@gmail.com>
2026-09-16 23:11:59 +0530

    Initial commit

--- src/ArrayProblems/Main.java
+++ src/ArrayProblems/Main.java
@@ -77,27 +77,51 @@ public class Main {
 //    }
 
     //Question-5
-    static int[] getPosNegSum(int arr[]){
-        int posSum = 0;
-        int negSum = 0;
+//    static int[] getPosNegSum(int arr[]){
+//        int posSum = 0;
+//        int negSum = 0;
+//        for (int i=0;i<arr.length;i++){
+//            if (arr[i]>0){
+//                //num is positive
+//                posSum = posSum + arr[i];
+//            }
+//            else {
+//                //num is negative
+//                negSum = negSum + arr[i];
+//            }
+//        }
+//        int ans[] = {posSum, negSum};
+//        return ans;
+//    }
+//
+//    static void main() {
+//        int arr[] = {5,-9,-3,10,-14,15};
+//        int ans[] = getPosNegSum(arr);
+//        System.out.println("Positive sum = " + ans[0]);
+//        System.out.println("Negative sum = " + ans[1]);
+//    }
+
+    //Question-6
+    static int[] getZeroOneCount(int arr[]){
+        int zeroCount = 0;
+        int oneCount = 0;
         for (int i=0;i<arr.length;i++){
-            if (arr[i]>0){
-                //num is positive
-                posSum = posSum + arr[i];
+            if (arr[i] == 0){
+                zeroCount++;
             }
             else {
-                //num is negative
-                negSum = negSum + arr[i];
+                //arr[i]=1
+                oneCount++;
             }
         }
-        int ans[] = {posSum, negSum};
+        int ans[] = {zeroCount, oneCount};
         return ans;
     }
 
     static void main() {
-        int arr[] = {5,-9,-3,10,-14,15};
-        int ans[] = getPosNegSum(arr);
-        System.out.println("Positive sum = " + ans[0]);
-        System.out.println("Negative sum = " + ans[1]);
+        int arr[] = {1,0,1,0,0,1,1,0};
+        int ans[] = getZeroOneCount(arr);
+        System.out.println("Zero count = " + ans[0]);
+        System.out.println("One count = " + ans[1]);
     }
 }
